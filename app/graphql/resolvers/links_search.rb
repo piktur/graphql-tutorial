e97f9075a74f1884e908ffd57ff13de715bfa248
Daniel Small <piktur.io@gmail.com>
2018-07-06 21:16:10 +1000

    ⭐️ new: search links

---
+++ app/graphql/resolvers/links_search.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'search_object/plugin/graphql'
+
+module Resolvers
+
+  class LinksSearch < GraphQL::Function
+
+    LinkFilter = GraphQL::InputObjectType.define do
+      name 'LinkFilter'
+
+      argument :OR, -> { types[LinkFilter] }
+      argument :description_contains, types.String
+      argument :url_contains, types.String
+    end
+
+    include SearchObject.module(:graphql)
+
+    scope { Link.all }
+
+    type !types[Types::LinkType]
+
+    option :filter, type: LinkFilter, with: :apply_filter
+
+    def apply_filter(scope, input)
+      branches = normalize_filters(input).reduce { |a, e| a.or(e) }
+      scope.merge(branches)
+    end
+
+    def normalize_filters(input, branches = [])
+      scope = Link.all
+      scope = scope.where('description LIKE ?', "%#{input['description_contains']}%") if input['description_contains']
+      scope = scope.where('url LIKE ?', "%#{input['url_contains']}%") if input['url_contains']
+
+      branches << scope
+
+      input['OR'].reduce(branches) { |a, e| normalize_filters(e, a) } if input['OR'].present?
+
+      branches
+    end
+
+  end
+
+end
+git commit  -m "✅ test: search links"
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,8 +3,5 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name 'Query'
 
-  field :allLinks, !types[Types::LinkType] do
-    description 'A list of Links'
-    resolve ->(_obj, _args, _ctx) { Link.all }
-  end
+  field :allLinks, function: Resolvers::LinksSearch
 end
