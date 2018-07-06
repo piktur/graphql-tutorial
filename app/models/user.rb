59a17bbb8294e7e064ce9278d53acb7a5dddd62c
Daniel Small <piktur.io@gmail.com>
2018-07-06 13:14:11 +1000

    👕 refactor: fix validation rules

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord # rubocop:disable Documentation
 
   has_secure_password
 
-  validate :name, presence: true
-  validate :email, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 
 end
